229849b06de975eef4aecc1daf1fbf9fdccdf2cb
YangHanBing <2180244540@qq.com>
2022-07-18 14:28:22 +0800

    feat(store/modules/user.js): 将用户信息存入vuex

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -7,12 +7,26 @@ import router from '../../router'
 export default {
   namespaced: true,
   state: {
-    token: getItem('token') || ''
+    token: getItem('token') || '',
+    userInfo: '',
+    menus: '',
+    ruleNames: ''
   },
   mutations: {
     setToken(state, token) {
       state.token = token
       setItem('token', token)
+    },
+    // 将用户信息存到vuex
+    setUserInfo(state, userInfo) {
+      const avatar = userInfo.avatar
+      const username = userInfo.username
+      state.userInfo = {
+        avatar,
+        username
+      }
+      state.menus = userInfo.menus
+      state.ruleNames = userInfo.ruleNames
     }
   },
   actions: {
@@ -31,6 +45,8 @@ export default {
     }) {
       const res = await User.getUserInfo()
       console.log(res)
+      commit('setUserInfo', res)
+      return res
     }
   }
 }
