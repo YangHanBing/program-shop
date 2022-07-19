25dbcb909b8f8fc453561dbf6e5a2a49375233fa
YangHanBing <2180244540@qq.com>
2022-07-19 08:15:53 +0800

    feat(navbar): 完善navbar图标样式

--- src/layout/AppNav.vue
+++ src/layout/AppNav.vue
@@ -6,9 +6,12 @@
     <div class="right">
       <div class="right_l">
         <!-- 折叠按钮 -->
-        <el-icon @click="changeMenus" class="hamburger hover-effect">
+        <!-- <el-icon @click="changeMenus" class="hamburger hover-effect">
           <svg-icon v-if="isCollapse" icon="hamburger-opened"></svg-icon>
           <svg-icon v-else icon="hamburger-closed"></svg-icon>
+        </el-icon> -->
+        <el-icon class="hamburger hover-effect">
+          <svg-icon icon="hamburger-opened"></svg-icon>
         </el-icon>
         <!-- 刷新按钮 -->
         <el-tooltip effect="dark" content="刷新" placement="bottom">
@@ -19,9 +22,11 @@
       </div>
       <div class="right_r">
         <!-- 全屏 -->
-        <el-icon class="icon">
-          <svg-icon :icon="isFullscreen ? 'aim' : 'screenfull'"></svg-icon>
-        </el-icon>
+        <el-tooltip effect="dark" content="全屏" placement="bottom">
+          <el-icon class="open">
+            <svg-icon icon="screenfull"></svg-icon>
+          </el-icon>
+        </el-tooltip>
         <el-dropdown @command="handleCommand">
           <span class="el-dropdown-link">
             <div class="user">
@@ -49,10 +54,13 @@
 import { useStore } from 'vuex'
 import { computed } from 'vue'
 import { ElMessage, ElMessageBox } from 'element-plus'
+import { ArrowDown } from '@element-plus/icons-vue'
 const store = useStore()
 const res = computed(() => {
   return store.getters.userInfo
 })
+// 刷新事件
+const refresh = () => {}
 // 功能事件
 const handleCommand = (command) => {
   switch (command) {
@@ -111,20 +119,34 @@ const handleLogout = () => {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  box-sizing: border-box;
+  padding-right: 20px;
   .right_l {
-    width: 100px;
+    width: 60px;
     height: 64px;
     display: flex;
     align-items: center;
-    justify-content: space-around;
+    justify-content: space-between;
+    .hamburger,
+    .reloadIcon {
+      padding: 24px 14px;
+    }
+    .hamburger:hover,
+    .reloadIcon:hover {
+      background-color: #4f46e5;
+    }
   }
   .right_r {
     width: 150px;
     height: 64px;
     display: flex;
     align-items: center;
-    .icon {
-      margin-right: 30px;
+    .open {
+      margin-right: 20px;
+      padding: 24px 14px;
+    }
+    .open:hover {
+      background-color: #4f46e5;
     }
     .user {
       display: flex;
