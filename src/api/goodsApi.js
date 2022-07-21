0c4eda2006474ad28f2012a7a59344a0df053daf
YangHanBing <2180244540@qq.com>
2022-07-21 19:51:33 +0800

    feat(商品管理): 封装商品管理对应接口

--- src/api/goodsApi.js
+++ src/api/goodsApi.js
@@ -14,7 +14,98 @@ const getGoodsType = () => {
     method: 'GET'
   })
 }
+// 新增商品接口
+const addGoods = (data) => {
+  return request({
+    url: '/admin/goods',
+    method: 'POST',
+    data
+  })
+}
+// 删除和批量删除接口
+const delGoods = (data) => {
+  return request({
+    url: '/goods/delete_all',
+    method: 'POST',
+    data
+  })
+}
+// 修改接口(禁止修改)
+const editGoods = (id, data) => {
+  return request({
+    url: `/admin/goods/${id}`,
+    method: 'POST',
+    data
+  })
+}
+// 上架,下架接口
+const changeStatusGoods = (data) => {
+  return request({
+    url: '/goods/changestatus',
+    method: 'POST',
+    data
+  })
+}
+// 获取商品信息 /admin/goods/read/48
+const readGoods = (data) => {
+  return request({
+    url: '/admin/goods/read',
+    method: 'GET',
+    data
+  })
+}
+// 更新商品规格 /admin/goods/updateskus/48
+const updateskusGoods = (id, data) => {
+  return request({
+    url: `/admin/goods/updateskus/${id}`,
+    method: 'POST',
+    data
+  })
+}
+// 设置商品轮播图接口 /admin/goods/banners/134
+const bannerGoods = (id, data) => {
+  return request({
+    url: `/admin/goods/banners/${id}`,
+    method: 'POST',
+    data
+  })
+}
+// 商品详情·接口 /admin/goods/134
+const descGoods = (id, data) => {
+  return request({
+    url: `/admin/goods/${id}`,
+    method: 'POST',
+    data
+  })
+}
+// 恢复商品接口
+const restoreGoods = (data) => {
+  return request({
+    url: '/admin/goods/restore',
+    method: 'POST',
+    data
+  })
+}
+// 彻底删除接口
+const destroyGoods = (data) => {
+  return request({
+    url: 'admin/goods/destroy',
+    method: 'POST',
+    data
+  })
+}
 export default {
   getGoodsList,
-  getGoodsType
+  getGoodsType,
+  addGoods,
+  delGoods,
+  editGoods,
+  changeStatusGoods,
+  readGoods,
+  updateskusGoods,
+  bannerGoods,
+  descGoods,
+
+  restoreGoods,
+  destroyGoods
 }
